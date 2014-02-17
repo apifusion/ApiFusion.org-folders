ed4dd4418e4d33bcb8d0793eae14d8d4308ba358
developer <developer@dev.apifusion.org>
2014-02-17 13:07:15 -0800

    404.php generated ns///index.xml

--- ns/404.php
+++ ns/404.php
@@ -1,8 +1,15 @@
 <?php
-//error_reporting( -1 );
-//ini_set( 'display_errors', 1 );
+error_reporting( -1 );
+ini_set( 'display_errors', 1 );
+
+$AF_defXmlName = 'index.xml';
 
 $url = $_SERVER["REQUEST_URI"];
+if( $AF_defXmlName == substr($url,0-strlen($AF_defXmlName)) )
+    $url = substr($url,0,0-strlen($AF_defXmlName));
+if( '/' == substr($url,-1) )
+    $url = substr($url,0,-1);
+
 $regEx = '/(.*)\/ns\/([^\/]*)\/(.*)/';
 //echo $regEx . "<hr/>";
 $webApp = preg_replace($regEx, '${1}', $url );
@@ -21,11 +28,39 @@ $_SERVER["REQUEST_URI"] = $uri;
 //$_SERVER["SCRIPT_NAME"] = $webApp . '/wiki/' . $wikiPage;
 //$_SERVER["PHP_SELF"] = $webApp . '/wiki/' . $wikiPage;
  
-//echo $uri;
 $wikiPath = realpath(__DIR__ . '/../wiki');
 putenv("MW_INSTALL_PATH=" . $wikiPath);
+
+$fdir = realpath( __DIR__ . $ns . '/' . $page);
+$xslPath = preg_replace("/[^\/]*\//", "../", $page. '/') . '../../ui/page.xsl';
+
+ob_start();
 require_once '../wiki/' . $wikiPage;
+$apiXml = ob_get_clean();
 
-//phpinfo();
+$xml = '<?xml version="1.0"  encoding="utf-8"?>' . "\r\n"
+	. '<?xml-stylesheet type="text/xsl" href="'. xmlEncode($xslPath) . '"?>'. "\r\n"
+	. '<ApiFusion>'. "\r\n"
+	. '	<wikiApiUrl>' . xmlEncode($uri) .'</wikiApiUrl>'. "\r\n"
+	. substr($apiXml, strpos($apiXml,'?>')+2) . "\r\n"
+	. '</ApiFusion>';
 
+$fdir =  __DIR__ .'/'. $ns . '/' . $page;
+
+if( strpos($apiXml, 'missingtitle') === false )
+{
+    mkdir( $fdir,  0777, true );
+    $xmlFileName = $fdir . '/' . $AF_defXmlName;
+    $f = fopen( $xmlFileName, 'w');
+    fwrite( $f, $xml );
+    fclose( $f );
+}
+
+//echo $xml;
+header('Location: ' . $url . '/', true, 301);
+
+function xmlEncode($str)
+{
+    return htmlspecialchars( $str, ENT_QUOTES, 'UTF-8');
+}
 ?>
