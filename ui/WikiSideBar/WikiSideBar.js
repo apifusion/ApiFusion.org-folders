e4734acce54d30df9898fad744b7394ba76eed2a
sashafirsov <suns@firsov.net>
2014-11-06 00:23:48 -0800

    ui/WikiSideBar namespaces shown

---
+++ ui/WikiSideBar/WikiSideBar.js
@@ -0,0 +1,79 @@
+define([ "dojo/query"	, "dojo/request", "dojo/_base/array"	,"dojo/store/Memory"	, "dijit/tree/ObjectStoreModel"	, "dijit/Tree"	, "dijit/form/CheckBox"	, "dojo/ready" ]
+, function( $			, request		, array					, Memory				, ObjectStoreModel				, Tree			, CheckBox				, ready )
+{
+	ready( function()
+	{
+		var afRoot = "/af/"
+		,	css=document.createElement("link");
+		css.setAttribute("rel", "stylesheet");
+		css.setAttribute("type", "text/css");
+		css.setAttribute("href", "https://ajax.googleapis.com/ajax/libs/dojo/1.10.1/dijit/themes/claro/claro.css");
+		document.getElementsByTagName("head")[0].appendChild(css)
+  		document.body.className += " claro";
+
+		request( afRoot+"ns/Namespaces.xml", {handleAs:"xml"} ).then( populateNS );
+    });
+	return {};
+	function
+populateNS( xml )
+{
+	var d = [{ id:"Default",name:"Default" }]
+	,	o = {};
+
+	array.forEach( xml.getElementsByTagName("ns"), function(el)
+	{	var e = {};
+		array.forEach( el.attributes, function( a ){ e[a.name] = a.value; });
+		e.name		= e.canonical;
+		e.parent	= e.category || 'Default';
+
+		var id = e.id * 1;
+		if( id<1 || id&1 )
+			return;
+		var cat = e.category;
+		if( cat && !( cat in o ) )
+			d.push( o[cat] = {id:cat,name:cat,parent:'Default'} );
+		d.push(e);
+	});
+	
+	var myStore = new Memory(
+	{   data: d
+	,   getChildren: function(object)
+		{	
+			return this.query({parent: object.id});	
+		}
+    });
+
+    var myModel = new ObjectStoreModel(
+	{   store: myStore
+    ,   query: {id: 'Default'}
+	,	mayHaveChildren: function(o)
+		{
+			return o.id == o.name;
+		}
+    });
+
+    var tree = new Tree
+	({  model	: myModel
+	,	showRoot: false
+	,	getIconClass: function(){return "";}
+	,	_createTreeNode: function(args)
+			{
+				var ret = new dijit._TreeNode(args);
+				ret.labelNode.innerHTML = args.label;
+
+				var cb = new CheckBox(
+				{	onChange: function(b)
+					{ 
+						ret.item.selected = b;
+						console.log( ret.item, b );
+					}
+				});
+				cb.placeAt( ret.labelNode, "first");
+
+				return ret;
+			}
+    });
+    tree.placeAt( $("#p-Namespaces-label a")[0] );
+    tree.startup();
+}
+});
