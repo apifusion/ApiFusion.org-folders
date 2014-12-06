a50d68143eb695b1d690be461b6777790d0738ad
sashafirsov <suns@firsov.net>
2014-12-05 22:10:54 -0800

    php/PageNS.php header could not be set error fix

--- php/PageNS.php
+++ php/PageNS.php
@@ -1,12 +1,12 @@
-[<?php
+<?php
 # get list of subpages beginning from current and up to root
 
 error_reporting(-1);
 ini_set('display_errors', 'On');
 putenv( 'MW_INSTALL_PATH='. realpath( dirname( __FILE__ ) . '/../wiki') );
+header('Content-Type: application/json');
 require realpath( dirname( __FILE__ ) . '/../wiki/includes/WebStart.php' );
 
-header('Content-Type: application/json');
 
 $fullTitle = str_replace ( ' ' , '_', $_REQUEST['title'] );
 $ns = '';
@@ -25,5 +25,7 @@ $dbr = wfGetDB( DB_MASTER );
 	$res = $dbr->query("select page_namespace as ns from wiki_page where page_title='$title' ");
 	foreach( $res as $row1 )
 	       array_push($nsArr, $row1->ns);	
+echo '[';
 	echo implode(',',$nsArr);
-?>]
+echo ']';
+?>
