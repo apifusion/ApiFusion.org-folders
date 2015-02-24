2765e09e665ec9835ee16c3b602f97a41308d103
sashafirsov <suns@firsov.net>
2015-02-23 21:15:16 -0800

    WindJetQuery API in xml.js

--- ui/AMD/xml.js
+++ ui/AMD/xml.js
@@ -7,6 +7,8 @@
 	// description:
 	//		As plugin returns XMLDOMDocument retrieved by XHR
 
+	var XHTML = "http://www.w3.org/1999/xhtml";
+
     return	{	load: function load(name, req, onLoad, config) 
 				{
 					getXml( req.toUrl( name ) , onLoad );
@@ -26,24 +28,61 @@
   
 					return node.selectSingleNode( xPath );//,nsmgr )
 				}
-			,	XPath_nl : function XPath_nl( xPath, node )
-				{
-					var d = node.ownerDocument || node
-					,	nsResolver = d.createNSResolver && d.createNSResolver(d.documentElement);
-					if( d.evaluate )
-						return (node.ownerDocument || node).evaluate( xPath, node, nsResolver, 0, null );
-
-					d.setProperty('SelectionLanguage', 'XPath');
-					d.setProperty('SelectionNamespaces', 'xmlns:xsl="http://www.w3.org/1999/XSL/Transform"');
-					return node.SelectNodes( xPath );//, nsmgr );
-				}
-			,	createElement : function createElement(name, document)
-				{
-					return document.createElementNS ? document.createElementNS(XHTML, name) : document.createElement(name);
-				}
+			,	XPath_nl : XPath_nl
+			,	$ : XPath_nl
+			,	createElement : createElement
 			,	cleanElement : cleanElement
 			};
+		function 
+	XPath_nl( xPath, node )
+	{
+		var d = node.ownerDocument || node
+		,	nsResolver = d.createNSResolver && d.createNSResolver(d.documentElement)
+		,	nl =[]
+		,	xr 
+		,	e;
+		if( d.evaluate )
+			xr = (node.ownerDocument || node).evaluate( xPath, node, nsResolver, 0, null );
+		else
+		{
+			d.setProperty('SelectionLanguage', 'XPath');
+			d.setProperty('SelectionNamespaces', 'xmlns:xsl="http://www.w3.org/1999/XSL/Transform"');
+			xr = node.SelectNodes( xPath );//, nsmgr );
+		}
+		while( e = xr.iterateNext() )
+			nl.push(e);
 
+		// WindJetQuery inlined
+		var o = nl[0] || createElement('b',d);
+
+		forEachProp( o, function(v,name)
+		{	
+			nl[name] = function 
+			invokeElementMethod()
+			{
+				var args	= arguments
+				,	i		= 0
+				,	max		= this.length;
+
+				this._ret = [];
+
+				for( ; i<max ; i++ )
+				{	var el	= this[i]
+					,	v	= el[name];
+					if( "function" === typeof v )
+						v = v.apply( el, args );
+					else if( args.length )	// setter
+						v = el[name] = args[ i % args.length ]; 
+					this._ret[i] = v;
+				}
+				return this;
+			}			
+		}, nl );
+
+		nl.attr = nl.setAttribute; // alias
+
+		return nl;
+	}
 		function 
 	getXml( url, callback )
 	{
@@ -77,9 +116,24 @@
 		cleanElement(el);
 		el.appendChild(p.transformToFragment(xml, document));
 	}
+	function createElement(name, document)
+	{
+		return document.createElementNS ? document.createElementNS(XHTML, name) : document.createElement(name);
+	}
 	function cleanElement(el)
 	{
 		while( el && el.lastChild)
 			el.removeChild(el.lastChild);
+	}
+
+		function
+	forEachProp( o, onProp, scope )
+	{
+		if( scope )
+			for( var n in o )
+				onProp.call( scope, o[n], n, o );
+		else
+			for( var n in o )
+				onProp( o[n], n, o );
 	}
 });
