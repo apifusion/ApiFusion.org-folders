e998fb43aaf5e3694fe1412a5e47f648de408f51
sashafir <sasha.firsov@johnmuirhealth.com>
2017-04-09 19:25:32 -0700

    AutoloadWidgets as widget and global AMD dependency

--- ui/WidgetLoader.js
+++ ui/WidgetLoader.js
@@ -5,24 +5,30 @@
 define(["jquery", "require"],function ( $, require )
 {   "use strict";
     // todo plugin API support for immediate page parsing "WidgetLoader!"
+    WidgetLoader.InitWidget = InitWidget;
     $(WidgetLoader);
     return WidgetLoader;
 
     function WidgetLoader()
     {
-        $("*[data-af-mid]").each( function InitWidget( i, el )
-        {   let mid = el.getAttribute("data-af-mid");
-            $(el).addClass( mid.toLowerCase().replace( /\//g, "-") );
-            require([mid], function CreateWidget( Widget )
-            {   try
-                {   let o = JSON.parse( el.getAttribute("data-af-param") );
-                    for( let k in o )
-                        if( o[k] && o[k].map )
-                            o[k] = o[k].map( v => v.trim() );
-                    new Widget( el, o );
-                }catch( ex )
-                    { console.error( mid, ex); debugger; }
-            });
+        $("*[data-af-mid]").each( (i,el) => InitWidget(el) );
+    }
+    function InitWidget( el )
+    {   let $el = $(el)
+        ,   mid = $el.attr("data-af-mid")
+        ,   cls = mid.toLowerCase().replace( /\//g, "-");
+        if( $el.hasClass(cls) )
+            return;
+        $el.addClass( cls );
+        require([mid], function CreateWidget( Widget )
+        {   try
+            {   let o = JSON.parse( $el.attr("data-af-param") || '{"skip":[""]}' );
+                for( let k in o )
+                    if( o[k] && o[k].map )
+                        o[k] = o[k].map( v => v.trim() );
+                new Widget( el, o );
+            }catch( ex )
+                { console.error( mid, ex); debugger; }
         });
     }
 });
