2f50355fc916723e8f29989830276e79dd3c25ae
sashafir <sasha.firsov@johnmuirhealth.com>
2017-07-09 17:32:36 -0700

    importGit.html pagesTotal added

--- ui/tools/importGit.js
+++ ui/tools/importGit.js
@@ -4,13 +4,13 @@ const urlParams={}; location.search.substring(1).split('&').filter(a=>!!a).forEa
 let gitRestfulUrl
 ,   wikiUrl
 ,   sourcesRoot
-,   orgs=[]
+,   orgs = []
 ,   lockedRepo
 ,   lockedBranch
 ,   afSourcesRoot
-,   wikiEditToken
-,   creationQueue = []
-,   pagesCreated = 0;
+,   creationQueue   = []
+,   pagesTotal      = 0
+,   pagesCreated    = 0;
 
 const SEARCH_URL = "/api.php?action=opensearch&format=xml&namespace=0&limit=1000&suggest=true&search=";
 const t2a = {}; // title 2 FolderEntry attributes
@@ -66,7 +66,7 @@ $onSubmit( 3, ()=>
 {
     afSourcesRoot = input('af-sources-root').val();
 //    enableStep( 4 );
-    setInterval( poolCreationQueue, 100 );
+    setInterval( poolCreationQueue, 300 );
     processFolder( '' );
     //const af = input('af-sources-root').val();
 });
@@ -78,13 +78,25 @@ poolCreationQueue()
         return;
     $('input[name=pages-created]').val( ++pagesCreated );
     $('input[name=pages-to-create]').val( creationQueue.length );
-    const text = JSON.stringify(r);
-    // tags
-    $.post( `${wikiUrl}/api.php`
-        ,`action=edit&summary=created&createonly=true&watchlist=watch&format=xml&token=${wikiEditToken}&titles=${r.title}&text=${text}`
-        , x => console.log(x) );
 
+    let text = encodeURIComponent( JSON.stringify(r) );
+    $.Xml( `${wikiUrl}/api.php?action=query&meta=tokens&type=csrf&format=xml&titles=${r.title}`)
+    .XPath( '//tokens' )
+    .$then( x=> $(x).attr('csrftoken') )
+    .$then( wikiEditToken => "+\\" === wikiEditToken &&  throwErr('wikiEditToken required') )
+    .$then( ( x, wikiEditToken ) => $.post( `${wikiUrl}/api.php`, `action=edit&summary=created&createonly=true&watchlist=watch&format=xml&token=${encodeURIComponent(wikiEditToken)}&title=${encodeURIComponent(r.title)}&text=${text}` ) )
+    .xPath( '//error|//edit' )
+    .$then( e =>
+            {   const c = $(e).attr('code');
+                if( 'articleexists' === c )
+                   return r.status = 'existing';
 
+                if( 'Success' === $(e).attr('result') ) // <edit new="" result="Success"
+                    return r.status = 'created';
+                throw $(e).attr('info');
+            })
+    .$then( x=> console.log('success', x ), err => console.error( r.info = err ), r.status = 'error' )
+    .$then( x=> renderPage( r ) );
 }
     function
 processFolder( folder )
@@ -108,44 +120,40 @@ processPages( pages )
         ,   k = t.substring( afSourcesRoot.length+1 )
         ,   r = t2a[ k.toLowerCase() ];
         r.title = t;
+        pagesTotal++;
         if( p.getAttribute( 'missing' ) !== '' )
         {   r.status = 'existing';
             renderPage(r);
+            r.isDirectory && processFolder( `${k}` );
             continue;
         }
+
         r.status = 'missing'; // todo do not process, mark previously deleted. Use action=query&prop=deletedrevisions
-        createPage(r).$then( r=> r.isDirectory && processFolder( `${k}` ) );
+        createPage(r).$then( r=>
+        {
+            r.isDirectory && processFolder( `${k}` )
+        });
     }
+    $('input[name=pages-total]').val( pagesTotal );
 }
     function
 renderPage( r )
 {
-    let status = { missing:'&times;', created:'*', existing:'&checkmark;', error:'!'}[r.status];
-    return $('.step4 table').append(`<tr title="${r.title}"><td><a href="${wikiUrl}/index.php/${r.title}">${r.title}</a></td><td>${status}</td><td>${status}</td></tr>`)
+    let status = { missing:'&times;', created:'*', existing:'&checkmark;', error:'!'}[r.status]
+    ,   type = r.isDirectory ? '.' : 'file';
+    return $('.step4 table').append(`<tr title="${r.title}"><td><a href="${wikiUrl}/index.php/${r.title}">${r.title}</a></td><td>${type}</td><td>${status}</td></tr>`)
         .$then( x=>r );
 }
     function
 createPage( r )
 {
     $('input[name=pages-to-create]').val( creationQueue.push(r) );
-    let text = encodeURIComponent( JSON.stringify(r) );
-    return $.Xml( `${wikiUrl}/api.php?action=query&meta=tokens&type=csrf&format=xml&titles=${r.title}`)
-    .XPath( '//tokens' )
-    .$then( x=> $(x).attr('csrftoken') )
-    .$then( wikiEditToken => "+\\" === wikiEditToken &&  throwErr('wikiEditToken required') )
-    .$then( ( x, wikiEditToken ) => $.post( `${wikiUrl}/api.php`, `action=edit&summary=created&createonly=true&watchlist=watch&format=xml&token=${encodeURIComponent(wikiEditToken)}&title=${encodeURIComponent(r.title)}&text=${text}` ) )
-    .xPath( '//error|//edit' )
-    .$then( e =>
-            {   const c = $(e).attr('code');
-                if( 'articleexists' === c )
-                   return r.status = 'existing';
-
-                if( 'Success' === $(e).attr('result') ) // <edit new="" result="Success"
-                    return r.status = 'created';
-                throw $(e).attr('info');
-            })
-    .$then( x=> console.log('success', x ), err => console.error( r.info = err ), r.status = 'error' )
-    .$then( x=> renderPage( r ) );
+    return $.$then(x=>r);
+}
+    function
+throwErr( err )
+{   console.log('throwErr', err );
+    throw err;
 }
     function
 $onSubmit( step, cb )
