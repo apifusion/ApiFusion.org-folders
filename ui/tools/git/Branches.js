d5824ac58ad228ab941ed67082f58cafd5333aa2
sashafir <sasha.firsov@johnmuirhealth.com>
2017-09-19 23:41:28 -0700

    Template:ApiFusion.org-folders/ui/tools/git/Branches

--- ui/tools/git/Branches.js
+++ ui/tools/git/Branches.js
@@ -4,7 +4,7 @@
  * http://www.ApiFusion.org/wiki/index.php/Template:ApiFusion.org-folders/ui/tools/git/Branches&action=edit
  * <span data-af-mid="af/ApiFusion.org-folders/ui/tools/git/Branches" data-af-param='{ "page":"{{{page|}}}" , "skip": [ "{{{skip|}}}"] }'></span>
  */
-define(['jquery','../Children'],function ( $, Children )
+define(['jquery'],function ( $ )
 {   "use strict";
     $('<style>.af-ui-tools-git-Branches a { background-color: rgba(128,128,0,.1); }</style>').appendTo('head');
     return function AfListBranches( node, params )
@@ -13,23 +13,16 @@ define(['jquery','../Children'],function ( $, Children )
         ,   wp  = mw.config.get('wgArticlePath')
         ,   cp  = mw.config.get('wgTitle')
         ,   pg  = ( "string" == typeof params ? params : params.page || cp )
-        ,   url = mw.config.get('wgScriptPath')+'/api.php?action=parse&prop=sections&page=Sources:'+ pg;
+        ,   url = mw.config.get('wgScriptPath')+'/api.php?action=parse&prop=sections&format=json&page=Sources:'+ pg;
         //  http://localhost/af/wiki/api.php?action=parse&prop=sections&page=ApiFusion.org/Modules
 
-        if( "object" !== typeof params  )
-            params = {};
-
-        params.skip = "string" === typeof params.skip
-                    ? params.skip.split(',')
-                    : [t];
-        params.page = pt;
         $.get( url, function( data )
         {
+            debugger;
             data.parse.sections.forEach( function( c )
             {   var t = c.line
                 ,   u = pg + '/' + t;
-                if( !skip.includes(t) )
-                    $w.append('<a class="btn" href="'+wp.replace('$1',u)+'">'+t+'</a> ');
+                $w.append('<a class="btn" href="'+wp.replace('$1',u)+'">'+t+'</a> ');
             });
         }).fail( function(err){  console.error( "error",err, url );  })
     };
